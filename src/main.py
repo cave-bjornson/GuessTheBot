fe27b77c2d6deb0f31ab94b51cc7f47b71c791ea
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-11-11 13:37:32 +0100

    fix: remove discord console
    The container instance on azure won't have a tty for stdin/out

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 import discord
 import ezcord
-from discord import Message, User, Member
+from discord import Message
 
-# from dpyConsole import Console
 from ezcord import log
 
-from models import PlayerDisplay
-from repository import GuesserDB, fernet
+from repository import GuesserDB
 
 db = GuesserDB()
 
@@ -18,20 +16,6 @@ bot = ezcord.Bot(intents=intents)
 @bot.listen()
 async def on_message(message: Message):
     log.info(message.content)
-
-
-@my_console.command()
-async def echo(text):
-    log.info(text)
-
-
-@my_console.command()
-async def players():
-    for p in await db.get_all_players():
-        d_id = int(fernet.decrypt(p.id_enc))
-        user: User = await bot.get_or_fetch_user(d_id)
-        pd = PlayerDisplay(name=user.display_name, join_date=p.join_date.date())
-        print(pd)
 
 
 if __name__ == "__main__":
